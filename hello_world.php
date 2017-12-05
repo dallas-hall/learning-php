5ee574dc28a3a88b58a3f97321ff504a2f6625eb
Dallas Hall <mr.dallas.hall@gmail.com>
2017-12-05 18:59:06 +1100

    updated permissions

--- hello_world.php
+++ hello_world.php
@@ -3,5 +3,5 @@
 	<title>A Noob Learning PHP</title>
 <head>
 <body>
-	<?php echo'<h1>My First PHP Program</h1><p>Hello world! =)<p>'; ?>
+	<?php echo'<h1>My First PHP Program</h1><p>Hello world! =)<p><hr />'; ?>
 </body>
