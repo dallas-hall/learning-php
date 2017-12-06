894526a293ed120bede8b0f7b4f68ed3968d66e9
Dallas Hall <mr.dallas.hall@gmail.com>
2017-12-06 21:45:57 +1100

    updated html for assignment 1

---
+++ wp/a2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE=html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Assignment 2</title>
+</head>
+<body>
+	<h1>Assignment 2</h1>
+	<p>
+	This page is under constructions.
+	</p>
+</body>
+
