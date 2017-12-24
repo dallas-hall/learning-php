c37aa04c31269495c2e4529c66f4c412b5d6373a
Dallas Hall <mr.dallas.hall@gmail.com>
2017-12-24 15:11:09 +1100

    updated the structure to match what was needed for the assignment

--- wp/a3/index.php
+++ wp/a3/index.php
@@ -2,12 +2,22 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Assignment 3</title>
+	<title>Assignment 2</title>
 </head>
 <body>
-	<h1>Assignment 3</h1>
-	<p>
-	This page is under constructions.
-	</p>
+<!-- This structure was taken from https://www.w3schools.com/html/html5_semantic_elements.asp -->
+	<header>
+		<h1>Assignment 3</h1>
+	</header>
+	<nav>
+		<p>No navigation stuff yet.</p>
+	</nav>
+	<main>
+		<p>This page is under construction.
+		<a href="brand-guideline.html" target="_blank">Brand Guideline from assignment 1</a></p>
+	</main>
+	<footer>
+		<p>This page was built by Dallas Hall.</p>
+	</footer>
 </body>
 
