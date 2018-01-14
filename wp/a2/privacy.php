e370769bf5f4577668add575668abfa5148aff9b
Dallas Hall <dallas.hall@example.com>
2018-01-14 12:53:34 +1100

    Updated code, added privacy, products, and contact pages.

---
+++ wp/a2/privacy.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Golden Tree Productions</title>
+	<link href="https://fonts.googleapis.com/css?family=Cardo|Josefin+Sans"
+		  rel="stylesheet">
+	<link href="css/master.css" type="text/css" rel="stylesheet">
+</head>
+<body id="privacy">
+<div class="wrapper">
+	<header>
+		<a id='home-link' href="index.php"><h1>Golden Tree Productions</h1><img
+					src="img/logos/gtp_new_logo_tree_only.png" width="50px"
+					height="50px"></a>
+		<nav id="top-nav">
+			<ul>
+				<li><a href="index.html" class="">Home</a></li>
+				<li><a href="#template-page" class="">Shop</a></li>
+				<li><a href="#template-page" class="">Contact</a></li>
+				<li><a href="#template-page" class="">Blog</a></li>
+				<li><a href="#template-page" class="">About</a></li>
+				<li><a href="#template-page" class="current">Template</a></li>
+			</ul>
+		</nav>
+		<fieldset id="members-area">
+			<legend>Members Area</legend>
+			<form action="">
+				<input id="buttonSignIn" type="button" name="sign-in"
+					   value="Sign In">
+				<input id="buttonSignUp" type="button" name="sign-up"
+					   value="Sign Up">
+			</form>
+		</fieldset>
+		<span class="breadcrumb">
+			<a href="index.php">Home</a>
+			<a href="privacy.php">Privacy</a>
+		</span>
+	</header>
+	<main>
+		<section>
+			<h2 class="main-content">Privacy Policy</h2>
+			<article>
+				<h3>Privacy Statement</h3>
+				<p>Amanda Hall @ Golden Tree Productions has created our privacy
+					policy to demonstrate our firm commitment to your privacy
+					and the protection of any information that you send or share
+					with us. All clients’ personal details and information is
+					considered extremely sensitive and very private.
+					Under no circumstance will your information be shared or
+					sold without your written permission.</p>
+				<h3>Protecting Your Privacy</h3>
+				<p>Amanda Hall @ Golden Tree Productions is operated by myself
+					and a small professional trusted team for website
+					administration and technical support. We only store minimal
+					information necessary for administration purposes. We use
+					security measures to protect against the loss, misuse and
+					alteration of any data stored by us. We never share,
+					sell, rent, lease or trade any details what so ever with
+					anyone. Information submitted to on our site or by
+					e-mail is only ever used for administration purposes.</p>
+				<p>If you would like further information about our policies,
+					please <a href="contact.php">contact us</a>.
+				</p>
+			</article>
+		</section>
+	</main>
+	<footer>
+		<h2>Additional Links</h2>
+		<a href="index.html"><img
+					src="img/logos/gtp_new_logo_tree_only.png" width="50px"
+					height="50px"></a>
+		<nav>
+			<ul>
+				<li><a href="template-split_page.html" class="">Returns &
+						Refunds</a></li>
+				<li><a href="template-split_page.html" class="">Terms &
+						Conditions</a></li>
+				<li><a href="template-split_page.html" class="">Privacy
+						Policy</a></li>
+				<li><a href="template-split_page.html" class="">Contact Us</a>
+				</li>
+			</ul>
+		</nav>
+		<div id="final-message">&copy; Golden Tree Productions,
+			<span id="current-year"></span>. Built by
+			Dallas Hall - s3461243.
+		</div>
+		<!-- Using JavaScript to add the current year into the page dynamically -->
+		<script type="text/javascript" src="js/getDate.js"></script>
+</div>
+</body>
+</html>
---
+++ wp/a2/products.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Golden Tree Productions</title>
+	<link href="https://fonts.googleapis.com/css?family=Cardo|Josefin+Sans"
+		  rel="stylesheet">
+	<link href="css/master.css" type="text/css" rel="stylesheet">
+</head>
+<body id="all-products-page">
+<div class="wrapper">
+	<header>
+		<a id='home-link'href="index.php"><h1>Golden Tree Productions</h1><img
+					src="img/logos/gtp_new_logo_tree_only.png" width="50px"
+					height="50px"></a>
+		<nav id="top-nav">
+			<ul>
+				<li><a href="index.php" class="">Home</a></li>
+				<li><a href="#template-page" class="current">Shop</a></li>
+				<li><a href="#template-page" class="">Contact</a></li>
+				<li><a href="#template-page" class="">Blog</a></li>
+				<li><a href="#template-page" class="">About</a></li>
+				<li><a href="t" class="">Template</a></li>
+			</ul>
+		</nav>
+		<fieldset id="members-area">
+			<legend>Members Area</legend>
+			<form action="">
+				<input id="buttonSignIn" type="button" name="sign-in"
+					   value="Sign In">
+				<input id="buttonSignUp" type="button" name="sign-up"
+					   value="Sign Up">
+			</form>
+		</fieldset>
+		<span class="breadcrumb">
+			<a href="index.php">Home</a>
+			<a href="products.php">Products</a>
+		</span>
+	</header>
+	<main>
+		<section>
+			<h2 class="main-content">Main Content</h2>
+			<article>
+
+			</article>
+		</section>
+		<aside id="side-content">
+			<h2>Side Content</h2>
+			<article>
+
+			</article>
+		</aside>
+	</main>
+	<footer>
+		<h2>Additional Links</h2>
+		<a href="index.html"><img
+				src="img/logos/gtp_new_logo_tree_only.png" width="50px"
+				height="50px"></a>
+		<nav>
+			<ul>
+				<li><a href="template-split_page.html" class="">Returns & Refunds</a></li>
+				<li><a href="template-split_page.html" class="">Terms & Conditions</a></li>
+				<li><a href="template-split_page.html" class="">Privacy Policy</a></li>
+				<li><a href="template-split_page.html" class="">Contact Us</a></li>
+			</ul>
+		</nav>
+		<div id="final-message">&copy; Golden Tree Productions,
+			<span id="current-year"></span>. Built by
+			Dallas Hall - s3461243.
+		</div>
+		<!-- Using JavaScript to add the current year into the page dynamically -->
+		<script type="text/javascript" src="js/getDate.js"></script>
+</div>
+</body>
+</html>
