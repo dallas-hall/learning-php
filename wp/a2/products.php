7bc598f55d776283b402a1e310e4e51724e2e8c1
Dallas Hall <dallas.hall@example.com>
2018-01-15 15:35:16 +1100

    Updated CSS

--- wp/a2/products.php
+++ wp/a2/products.php
@@ -38,68 +38,70 @@
 		</span>
 	</header>
 	<main id="singlePage">
-		<h2>All Products</h2>
-		<section id="videoServices">
-			<h3>Video Services</h3>
+		<h2>Golden Tree Production Products &amp; Services</h2>
+		<section id="videoServices" class="allProducts">
+			<h3>Video Products &amp; Services</h3>
 			<article id="videoTranfser" class="allProducts">
-			<h4>Video Transfer</h4>
-			<img src="img/people/jack_and_equipment.jpg" width="256px"
-				 height="170px">
-			<p>The video transfer services we provide are:</p>
-			<ul>
-				<li>Transfer VHS or Super 8 to CD.</li>
-				<li>Transfer VHS or Super 8 to DVD.</li>
-				<li>Transfer VHS or Super 8 to USB.</li>
-				<li>Transfer VHS or Super 8 to SD Card.</li>
-			</ul>
-			<p>All video transfer services cost $30.00 AUD.</p>
-			<input type="button" name="transferMoreDetails"
-				   value="More Details">
-			<input type="button" name="buyTransfer" value="Buy Now">
+				<h4>Video Transfer</h4>
+				<img src="img/people/jack_and_equipment.jpg" width="256px"
+					 height="170px">
+				<p>The video transfer services we provide are:</p>
+				<ul>
+					<li>Transfer VHS or Super 8 to CD.</li>
+					<li>Transfer VHS or Super 8 to DVD.</li>
+					<li>Transfer VHS or Super 8 to USB.</li>
+					<li>Transfer VHS or Super 8 to SD Card.</li>
+				</ul>
+				<p>All video transfer services cost $30.00 AUD.</p>
+				<input type="button" name="transferMoreDetails"
+					   value="More Details">
+				<input type="button" name="buyTransfer" value="Buy Now">
 			</article>
-		<article id="videoProduction" class="allProducts">
-			<h4>Video Production</h4>
-			<img src="img/people/jack_and_client.jpg" width="256px"
-				 height="170px">
-			<p>The video productions services we provide are:</p>
-			<ul>
-				<li>Filming &amp; editing</li>
-				<li>Pre &amp; post production</li>
-				<li>Photography (using film stills)</li>
-			</ul>
-			<p>Please contact us with your job's details so we can
-				arrange a free and obligation free quote. Sorry, but we
-				do not do weddings due to high insurance costs.</p>
-			<input type="button" name="productionMoreDetails"
-				   value="More Details">
-			<input type="button" name="contactProduction" value="Contact Us">
-		</article>
-		<section id="psychicServices">
-			<h3>Psychic Services</h3>
+			<article id="videoProduction" class="allProducts">
+				<h4>Video Production</h4>
+				<img src="img/people/jack_and_client.jpg" width="256px"
+					 height="170px">
+				<p>The video productions services we provide are:</p>
+				<ul>
+					<li>Filming &amp; editing</li>
+					<li>Pre &amp; post production</li>
+					<li>Photography (using film stills)</li>
+				</ul>
+				<p>Please contact us with your job's details so we can
+					arrange a free and obligation free quote. Sorry, but we
+					do not do weddings due to high insurance costs.</p>
+				<input type="button" name="productionMoreDetails"
+					   value="More Details">
+				<input type="button" name="contactProduction"
+					   value="Contact Us">
+			</article>
+		</section>
+		<section id="psychicServices" class="allProducts">
+			<h3>Psychic Products &amp; Services</h3>
 			<p><b>Note:</b> All products here have free shipping within
 				Australia and a flat fee of $45.00 AUD for international
 				shipping.</p>
-			<section id="simplyTarot">
+			<section id="simplyTarot" class="allProducts">
 				<h4>Simply Tarot</h4>
-				<article id="simplyTarotKitsWithDVD" class="allProducts">
-					<h5>Simply Tarot Kit With DVD</h5>
+				<article id="simplyTarotSets" class="allProducts">
+					<h5>Simply Tarot Sets</h5>
 					<img src="img/products/psychic/tarot/simply/simply_dvd_book_and_card.jpg"
 						 width="256px"
 						 height="170px">
-					<p>This Simply Tarot Kit includes:</p>
-					<ul>
-						<li>Simply Tarot Training DVD.</li>
-						<li>Simply Tarot Training Book.</li>
-						<li>Simply Tarot Card Set.</li>
-					</ul>
-					<p>This set is limited edition and will cost $103.00
-						AUD.</p>
+					<p>There are 2 Simply Tarot Sets.</p>
+					<ol>
+						<li>Simply Tarot Set with the book, cards, and
+							the DVD</li>
+						<li>Simply Tarot Set the book and cards.</li>
+					</ol>
+					<p>The Simply Tarot set with the DVD is $103.00 AUD and
+						the Simply Tarot set without the DVD is $83.00 AUD.</p>
 					<input type="button" name="simplyTarotDVDMoreDetails"
 						   value="More Details">
 					<input type="button" name="buySimplyTarotDVD"
 						   value="Buy Now">
 				</article>
-				<article id="simplyTarotKitsNoVD" class="allProducts">
+<!--				<article id="simplyTarotKitsNoVD" class="allProducts">
 					<h5>Simply Tarot Kit Without DVD</h5>
 					<img src="img/products/psychic/tarot/simply/simply_book_and_card.jpg"
 						 width="256px"
@@ -113,13 +115,13 @@
 					<input type="button" name="simplyTarotMoreDetails"
 						   value="More Details">
 					<input type="button" name="buySimplyTarot" value="Buy Now">
-				</article>
+				</article>-->
 				<article id="simplyTarotCandles" class="allProducts">
 					<h5>Simply Tarot Candles</h5>
 					<img src="img/products/psychic/candles/simply/3_small_simply_candle.jpg"
 						 width="256px"
 						 height="170px">
-					<p>There are 2 types of Simply Tarot Candles</p>
+					<p>There are 2 types of Simply Tarot Candles.</p>
 					<ol>
 						<li>One Large Simply Tarot Candle</li>
 						<li>Three Small Simply Tarot Candles.</li>
@@ -134,12 +136,12 @@
 			</section>
 			<section id="secretsOfTarot" class="allProducts">
 				<h4>Secrets Of Tarot</h4>
-				<article id="secretsOfTarotKits" class="allProducts">
-					<h5>Simply Tarot Kit</h5>
+				<article id="secretsOfTarotSet" class="allProducts">
+					<h5>Simply Tarot Set</h5>
 					<img src="img/products/psychic/tarot/secrets/secrets_book_and_card.jpg"
 						 width="256px"
 						 height="170px">
-					<p>This Secrets Of Tarot Kit includes:</p>
+					<p>This Secrets Of Tarot Set includes:</p>
 					<ul>
 						<li>Simply Tarot Training Book.</li>
 						<li>Simply Tarot Card Set.</li>
