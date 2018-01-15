21d17c1d0f05aad7f1f6a2298ec4d4d724c457d1
Dallas Hall <mr.dallas.hall@gmail.com>
2018-01-15 11:55:34 +1100

    Added all products page and styled it.

--- wp/a2/products.php
+++ wp/a2/products.php
@@ -38,11 +38,137 @@
 		</span>
 	</header>
 	<main id="singlePage">
-		<section class="mainContent">
-			<h2>Main Content</h2>
-			<article>
-
+		<h2>All Products</h2>
+		<section id="videoServices">
+			<h3>Video Services</h3>
+			<article id="videoTranfser" class="allProducts">
+			<h4>Video Transfer</h4>
+			<img src="img/people/jack_and_equipment.jpg" width="256px"
+				 height="170px">
+			<p>The video transfer services we provide are:</p>
+			<ul>
+				<li>Transfer VHS or Super 8 to CD.</li>
+				<li>Transfer VHS or Super 8 to DVD.</li>
+				<li>Transfer VHS or Super 8 to USB.</li>
+				<li>Transfer VHS or Super 8 to SD Card.</li>
+			</ul>
+			<p>All video transfer services cost $30.00 AUD.</p>
+			<input type="button" name="transferMoreDetails"
+				   value="More Details">
+			<input type="button" name="buyTransfer" value="Buy Now">
 			</article>
+		<article id="videoProduction" class="allProducts">
+			<h4>Video Production</h4>
+			<img src="img/people/jack_and_client.jpg" width="256px"
+				 height="170px">
+			<p>The video productions services we provide are:</p>
+			<ul>
+				<li>Filming &amp; editing</li>
+				<li>Pre &amp; post production</li>
+				<li>Photography (using film stills)</li>
+				<li>Transfer VHS or Super 8 to SD Card.</li>
+			</ul>
+			<p>Please contact us with your job's details so we can
+				arrange a free and obligation free quote. Sorry, but we
+				do not do weddings due to high insurance costs.</p>
+			<input type="button" name="productionMoreDetails"
+				   value="More Details">
+			<input type="button" name="buyProduction" value="Buy Now">
+		</article>
+		<section id="psychicServices">
+			<h3>Psychic Services</h3>
+			<p><b>Note:</b> All products here have free shipping within
+				Australia and a flat fee of $45.00 AUD for international
+				shipping.</p>
+			<section id="simplyTarot">
+				<h4>Simply Tarot</h4>
+				<article id="simplyTarotKitsWithDVD" class="allProducts">
+					<h5>Simply Tarot Kit With DVD</h5>
+					<img src="img/products/psychic/tarot/simply/simply_dvd_book_and_card.jpg"
+						 width="256px"
+						 height="170px">
+					<p>This Simply Tarot Kit includes:</p>
+					<ul>
+						<li>Simply Tarot Training DVD.</li>
+						<li>Simply Tarot Training Book.</li>
+						<li>Simply Tarot Card Set.</li>
+					</ul>
+					<p>This set is limited edition and will cost $103.00
+						AUD.</p>
+					<input type="button" name="simplyTarotDVDMoreDetails"
+						   value="More Details">
+					<input type="button" name="buySimplyTarotDVD"
+						   value="Buy Now">
+				</article>
+				<article id="simplyTarotKitsNoVD" class="allProducts">
+					<h5>Simply Tarot Kit Without DVD</h5>
+					<img src="img/products/psychic/tarot/simply/simply_book_and_card.jpg"
+						 width="256px"
+						 height="170px">
+					<p>This Simply Tarot Kit includes:</p>
+					<ul>
+						<li>Simply Tarot Training Book.</li>
+						<li>Simply Tarot Card Set.</li>
+					</ul>
+					<p>This set is limited edition and will cost $83.00 AUD.</p>
+					<input type="button" name="simplyTarotMoreDetails"
+						   value="More Details">
+					<input type="button" name="buySimplyTarot" value="Buy Now">
+				</article>
+				<article id="simplyTarotCandles" class="allProducts">
+					<h5>Simply Tarot Candles</h5>
+					<img src="img/products/psychic/candles/simply/3_small_simply_candle.jpg"
+						 width="256px"
+						 height="170px">
+					<p>There are 2 types of Simply Tarot Candles</p>
+					<ol>
+						<li>One Large Simply Tarot Candle</li>
+						<li>Three Small Simply Tarot Candles.</li>
+					</ol>
+					<p>The single large candle is $57.95 AUD and the gift
+						pack containing 3 small candles are $49.95 AUD.</p>
+					<input type="button" name="simplyTarotCandlesMoreDetails"
+						   value="More Details">
+					<input type="button" name="buySimplyTarotCandles"
+						   value="Buy Now">
+				</article>
+			</section>
+			<section id="secretsOfTarot" class="allProducts">
+				<h4>Secrets Of Tarot</h4>
+				<article id="secretsOfTarotKits" class="allProducts">
+					<h5>Simply Tarot Kit</h5>
+					<img src="img/products/psychic/tarot/secrets/secrets_book_and_card.jpg"
+						 width="256px"
+						 height="170px">
+					<p>This Secrets Of Tarot Kit includes:</p>
+					<ul>
+						<li>Simply Tarot Training Book.</li>
+						<li>Simply Tarot Card Set.</li>
+					</ul>
+					<p>This set is limited edition and will cost $49.95 AUD.</p>
+					<input type="button" name="secretsOfTarotDVDMoreDetails"
+						   value="More Details">
+					<input type="button" name="buySecretsOfTarot"
+						   value="Buy Now">
+				</article>
+				<article id="secretsOfTarotCandles" class="allProducts">
+					<h5>Simply Tarot Candles</h5>
+					<img src="img/products/psychic/candles/secrets/large_secrets_candle.jpg"
+						 width="210px"
+						 height="244">
+					<p>There are 2 types of Secrets Of Tarot Candles</p>
+					<ol>
+						<li>One Large Secrets Of Tarot Candle</li>
+						<li>Three Small Secrets Of Tarot Candles.</li>
+					</ol>
+					<p>The single large candle is $57.95 AUD and the gift
+						pack containing 3 small candles are $49.95 AUD.</p>
+					<input type="button" name="secretsOfTarotCandlesMoreDetails"
+						   value="More Details">
+					<input type="button" name="buySecretsOfTarotCandles"
+						   value="Buy Now">
+				</article>
+			</section>
 		</section>
 	</main>
 	<footer>
