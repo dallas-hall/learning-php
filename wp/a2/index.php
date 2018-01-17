f9476a466182dc273fe654c65ebd7aede94cc0a0
Dallas Hall <dallas.hall@example.com>
2018-01-17 20:35:04 +1100

    Styled nav bar links and image links.

--- wp/a2/index.php
+++ wp/a2/index.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<!--Setting page information, which I learnt in the course textbook and also at https://www.w3schools.com/tags/tag_meta.asp-->
 	<meta charset="UTF-8">
+	<meta name="author" content="Dallas Hall">
+	<meta name="keywords" content="Golden Tree Productions,Amanda Hall,Jack Egerton,Simply Tarot,Secrets of Tarot,
+	Tarot,Astrology,Psychic,Video Production,Video,Candle,Candles">
+	<meta name="description" content="This is the home page for Golden Tree Productions, which sells video production
+	 services and tarot products and services, such as Simply Tarot and Secrets of Tarot.">
 	<title>Golden Tree Productions</title>
 	<link href="https://fonts.googleapis.com/css?family=Cardo|Josefin+Sans" rel="stylesheet">
 	<link href="css/master.css" type="text/css" rel="stylesheet">
 </head>
 <body>
+<!--The page design layout was sourced from the course textbook, using concepts discussed in chapter 15, such as the
+960 grid design pattern and above/below the fold. But particularly the layout of the end of chapter example in chapter
+ 17 was used as a starting point.-->
 <div class="pageWrapper" id="indexPage">
 	<header>
-		<a href="index.php"><img src="img/logos/gtp_new_logo_tree_only.png" width="50px" height="50px">
+		<a class="navigation_image" href="index.php"><img src="img/logos/gtp_new_logo_tree_only.png" width="50px"
+														  height="50px"
+														  alt="The new logo of Golden Tree Productions. It has a golden tree sprouting out of what looks a book.">
 			<h1>Golden Tree Productions</h1></a>
+		<!--The navigation menus were designed using the course textbook chapter 18.-->
 		<nav id="topNavigation">
 			<ul>
 				<li><a href="index.php" class="current">Home</a></li>
@@ -20,6 +32,7 @@
 				<li><a href="#bottomNavigation" class="">Bottom</a></li>
 			</ul>
 		</nav>
+		<!--The idea of using a fieldset was sourced from course textbook, chapter 7.-->
 		<fieldset id="membersArea">
 			<legend>Members Area</legend>
 			<form action="login.php" class="memberButtons">
@@ -34,13 +47,16 @@
 	<main id="singlePage">
 		<section class="aboveTheFold">
 			<p>Welcome to...</p>
-			<img src="img/logos/gtp_new_logo_half_size.png">
+			<img src="img/logos/gtp_new_logo_half_size.png" alt="The new logo of Golden Tree Productions. It has a
+			golden tree sprouting out of what looks a book with the words Golden Tree Production written underneath.">
 			<p>This is a place to learn about yourself and the universe. And capture it too!</p>
 			<div id="servicesLinks"><a href="products.php#videoServices" class="productsLink">
-					<img src="img/people/jack_with_camera.jpg" width="115px" height="100px">
+					<img src="img/people/jack_with_camera.jpg" width="115px" height="100px" alt="Jack Egerton with a
+					video camera.">
 					<p>Video Products &amp; Services</p></a>
 				<a href="products.php#psychicServices" class="productsLink">
-					<img src="img/people/amanda_and_cards.jpg" width="130px" height="100px">
+					<img src="img/people/amanda_and_cards.jpg" width="130px" height="100px" alt="Amanda Hall with a
+					spread of Tarot cards on the table, and a single big sized promotional card next to her.">
 					<p>Psychic Products &amp; Services</p></a>
 			</div>
 			<hr>
@@ -92,7 +108,9 @@
 	</main>
 	<footer>
 		<h2>Additional Links</h2>
-		<a href="index.php"><img src="img/logos/gtp_new_logo_tree_only.png" width="50px" height="50px"></a>
+		<a class="navigation_image" href="index.php"><img src="img/logos/gtp_new_logo_tree_only.png" width="50px"
+														  height="50px"
+														  alt="The new logo of Golden Tree Productions. It has a golden tree sprouting out of what looks a book.">
 		<nav id="bottomNavigation">
 			<ul>
 				<li><a href="about_us.php" class="">About</a></li>
@@ -106,7 +124,8 @@
 		<div id="finalMessage">&copy; Golden Tree Productions,
 			<span id="currentYear"></span>. Built by Dallas Hall - s3461243.
 		</div>
-		<!-- Using JavaScript to add the current year into the page dynamically -->
+		<!-- Using JavaScript to add the current year into the page dynamically, this idea was taken from the unit's
+		week 5 (?) lecture.-->
 		<script type="text/javascript" src="js/getDate.js"></script>
 	</footer>
 </div>
