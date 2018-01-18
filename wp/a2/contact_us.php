5656e27c8c3e8977245dd9b56793ca0aaed71318
Dallas Hall <mr.dallas.hall@gmail.com>
2018-01-19 07:52:57 +1100

    Fixed the completely broken CSS

--- wp/a2/contact_us.php
+++ wp/a2/contact_us.php
@@ -65,78 +65,76 @@
 			</article>
 			<aside class="column2">
 				<h2>Contact Us</h2>
-				<article>
-					<h3>Jack</h3>
-					<table>
-						<thead></thead>
-						<tbody>
-						<tr>
-							<th scope="row">Phone:</th>
-							<td>
-								<address>0423-574-669</address>
-							</td>
-						</tr>
-						<tr>
-							<th scope="row">Email:</th>
-							<td>
-								<address>
-									<a class="email"
-									   href="mailto:jack@goldentree.com.au?Subject=Golden%20Tree%20Video%20Question">jack@goldentree.com.au
-									</a>
-								</address>
-							</td>
-						</tr>
-						</tbody>
-						<tfoot></tfoot>
-					</table>
-					<h3>Amanda</h3>
-					<table>
-						<thead></thead>
-						<tbody>
-						<tr>
-							<th scope="row">Phone:</th>
-							<td>
-								<address>0412-435-558</address>
-							</td>
-						</tr>
-						<tr>
-							<th scope="row">Email:</th>
-							<td>
-								<address>
-									<a class="email"
-									   href="mailto:amanda@goldentree.com.au?Subject=Golden%20Tree%20Tarot%20Question"
-									>amanda@goldentree.com.au
-									</a>
-								</address>
-							</td>
-						</tr>
-						</tbody>
-						<tfoot></tfoot>
-					</table>
-					<h3>Operating Hours</h3>
-					<table>
-						<thead></thead>
-						<tbody>
-						<tr>
-							<th scope="row">Mon-Fri</th>
-							<td>9am-5pm</td>
-						</tr>
-						<tr>
-							<th scope="row">Sat</th>
-							<td>10am-4pm</td>
-						</tr>
-						<tr>
-							<th scope="row">Sun</th>
-							<td>Closed</td>
-						</tr>
-						</tbody>
-						<tfoot></tfoot>
-					</table>
-					<h3>Business Location</h3>
-					<p>Elanora, Queensland, Australia.</p>
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28146.056913179313!2d153.43400975368306!3d-28.138660037360847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b91026870e4b43f%3A0x502a35af3dea2a0!2sElanora+QLD+4221!5e0!3m2!1sen!2sau!4v1515917209715"
-							width="320" height="240" frameborder="0" style="border:0" allowfullscreen></iframe>
-				</article>
+				<h3>Jack</h3>
+				<table>
+					<thead></thead>
+					<tbody>
+					<tr>
+						<th scope="row">Phone:</th>
+						<td>
+							<address>0423-574-669</address>
+						</td>
+					</tr>
+					<tr>
+						<th scope="row">Email:</th>
+						<td>
+							<address>
+								<a class="email"
+								   href="mailto:jack@goldentree.com.au?Subject=Golden%20Tree%20Video%20Question">jack@goldentree.com.au
+								</a>
+							</address>
+						</td>
+					</tr>
+					</tbody>
+					<tfoot></tfoot>
+				</table>
+				<h3>Amanda</h3>
+				<table>
+					<thead></thead>
+					<tbody>
+					<tr>
+						<th scope="row">Phone:</th>
+						<td>
+							<address>0412-435-558</address>
+						</td>
+					</tr>
+					<tr>
+						<th scope="row">Email:</th>
+						<td>
+							<address>
+								<a class="email"
+								   href="mailto:amanda@goldentree.com.au?Subject=Golden%20Tree%20Tarot%20Question"
+								>amanda@goldentree.com.au
+								</a>
+							</address>
+						</td>
+					</tr>
+					</tbody>
+					<tfoot></tfoot>
+				</table>
+				<h3>Operating Hours</h3>
+				<table>
+					<thead></thead>
+					<tbody>
+					<tr>
+						<th scope="row">Mon-Fri</th>
+						<td>9am-5pm</td>
+					</tr>
+					<tr>
+						<th scope="row">Sat</th>
+						<td>10am-4pm</td>
+					</tr>
+					<tr>
+						<th scope="row">Sun</th>
+						<td>Closed</td>
+					</tr>
+					</tbody>
+					<tfoot></tfoot>
+				</table>
+				<h3>Business Location</h3>
+				<p>Elanora, Queensland, Australia.</p>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28146.056913179313!2d153.43400975368306!3d-28.138660037360847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b91026870e4b43f%3A0x502a35af3dea2a0!2sElanora+QLD+4221!5e0!3m2!1sen!2sau!4v1515917209715"
+						width="320" height="240" frameborder="0" style="border:0" allowfullscreen></iframe>
 			</aside>
 		</main>
 	</section>
