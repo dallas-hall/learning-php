0568ba367f813e0f151024cfd80f8db7dc8ff035
Dallas Hall <dallas.hall@example.com>
2018-02-10 09:27:51 +1100

    Added missing product.

--- wp/a3/product_script.php
+++ wp/a3/product_script.php
@@ -19,10 +19,17 @@ $productsTree = array (
 		,'productImage' => 'img/products/video/vcr.jpg'
 		,'productImageText' => 'A picture of a VHS player, some remotes, and various types of tapes.'
 		,'productDescription' => 'In today’s fast moving world our lives move so fast and memories fade just as quick. We all have some old tapes locked in a cupboard that we are going to do something with. Well today is the day . Get them out sort through them. What is special and you want to keep for future generations. They make a lovely gift to give. Being able to share memories from family history, special occasions, weddings, christenings, or even the family just having fun. We can transfer old VHS and Super 8 to CD, DVD, SD Card, or USB memory stick.'
-		,'normalPrice' => '30.00'
-		,'salePrice' => '0.00'
-		/*,'sourceTypes' => array ('vhs', 'super8')
-		,'targetTypes' => array ('cd', 'dvd', 'usb', 'sdcard')*/
+		,'price' => array (
+			'hasPrice' => true
+			,'shopPrice' => array (
+				'normalPrice' => '30.00'
+				,'salePrice' => '0.00'
+			)
+			,'postagePrice' => array (
+				'domesticPrice' => '0.00'
+				,'internationalPrice' => '0.00'
+			)
+		)
 		,'Testimonials' => array (
 			'Quotes' => array (
 				'<i>"Jack you are awesome. The music clips are just so awesome.– Love them all big fella."</i><br>Roger Pearson - Musician &amp; song writer.'
@@ -34,13 +41,49 @@ $productsTree = array (
 		)
 	)
 
+	,'videoProduction' => array (
+	'productTitle' => 'Video Production'
+	,'productImage' => 'img/products/video/vcr.jpg'
+	,'productImageText' => 'A picture of a VHS player, some remotes, and various types of tapes.'
+	,'productDescription' => 'Contact us for a detailed free quote.'
+	,'price' => array (
+		'hasPrice' => false
+		,'shopPrice' => array (
+			'normalPrice' => '0.00'
+			,'salePrice' => '0.00'
+		)
+		,'postagePrice' => array (
+			'domesticPrice' => '0.00'
+			,'internationalPrice' => '0.00'
+		)
+	)
+,'Testimonials' => array (
+		'Quotes' => array (
+			'<i>"Jack you are awesome. The music clips are just so awesome.– Love them all big fella."</i><br>Roger Pearson - Musician &amp; song writer.'
+		)
+	,'Samples' => array (
+			'<iframe width="320" height="240" src="https://www.youtube.com/embed/N9BhV2Pt4aI" frameborder="0" allowfullscreen></iframe>'
+		,'<iframe width="320" height="240" src="https://www.youtube.com/embed/U0ifHJfq5RI" frameborder="0" allowfullscreen></iframe>'
+		)
+	)
+)
+	
 	,'simplyTarotSetDVD' => array (
 		'productTitle' => 'Simply Tarot Set With DVD'
 		,'productImage' => 'img/products/psychic/tarot/simply/simply_dvd_book_and_card.jpg'
 		,'productImageText' => ''
 		,'productDescription' => ''
-		,'normalPrice' => '103.00'
-		,'salePrice' => '0.00'
+		,'price' => array (
+			'hasPrice' => true
+			,'shopPrice' => array (
+				'normalPrice' => '103.00'
+				,'salePrice' => '0.00'
+			)
+			,'postagePrice' => array (
+				'domesticPrice' => '0.00'
+				,'internationalPrice' => '45.00'
+			)
+		)
 		,'Testimonials' => array (
 			'Quotes' => array (
 				'<i>"Your cards helped me to become a professional reader. I have a massive soft spot for the cards. And the DVD that came with them helped too. I just can’t say a big enough thank you - xxx. You certainly can place my comment on the site. They have got a magic. Really lovely. I recommend them to others who are new to tarot also x"</i> - Sophie'
@@ -56,8 +99,17 @@ $productsTree = array (
 		,'productImage' => 'img/products/psychic/tarot/simply/simply_book_and_card.jpg'
 		,'productImageText' => ''
 		,'productDescription' => ''
-		,'normalPrice' => '83.00'
-		,'salePrice' => '0.00'
+		,'price' => array (
+			'hasPrice' => true
+			,'shopPrice' => array (
+				'normalPrice' => '83.00'
+				,'salePrice' => '0.00'
+			)
+			,'postagePrice' => array (
+				'domesticPrice' => '0.00'
+				,'internationalPrice' => '45.00'
+			)
+		)
 		,'Testimonials' => array (
 			'Quotes' => array (
 				'<i>"Your cards helped me to become a professional reader. I have a massive soft spot for the cards. And the DVD that came with them helped too. I just can’t say a big enough thank you - xxx. You certainly can place my comment on the site. They have got a magic. Really lovely. I recommend them to others who are new to tarot also x"</i> - Sophie'
@@ -73,8 +125,17 @@ $productsTree = array (
 		,'productImage' => 'img/products/psychic/candles/simply/large_simply_candle.jpg'
 		,'productImageText' => ''
 		,'productDescription' => 'In today’s fast moving world our lives move so fast and memories fade just as quick. We all have some old tapes locked in a cupboard that we are going to do something with. Well today is the day . Get them out sort through them. What is special and you want to keep for future generations. They make a lovely gift to give. Being able to share memories from family history, special occasions, weddings, christenings, or even the family just having fun. We can transfer old VHS and Super 8 to CD, DVD, SD Card, or USB memory stick.'
-		,'normalPrice' => '57.95'
-		,'salePrice' => '0.00'
+		,'price' => array (
+			'hasPrice' => true
+			,'shopPrice' => array (
+				'normalPrice' => '57.95'
+				,'salePrice' => '0.00'
+			)
+			,'postagePrice' => array (
+				'domesticPrice' => '0.00'
+				,'internationalPrice' => '45.00'
+			)
+		)
 		,'Testimonials' => array (
 			'Quotes' => array (
 				'<i>"It was so lovely to see you Amanda and your beautiful new candle range they’re amazing.</i>" - Maz Rukz '
@@ -94,15 +155,24 @@ $productsTree = array (
 		,'productImage' => 'img/products/psychic/candles/simply/3_small_simply_candle.jpg'
 		,'productImageText' => ''
 		,'productDescription' => 'In today’s fast moving world our lives move so fast and memories fade just as quick. We all have some old tapes locked in a cupboard that we are going to do something with. Well today is the day . Get them out sort through them. What is special and you want to keep for future generations. They make a lovely gift to give. Being able to share memories from family history, special occasions, weddings, christenings, or even the family just having fun. We can transfer old VHS and Super 8 to CD, DVD, SD Card, or USB memory stick.'
-		,'normalPrice' => '49.95'
-		,'salePrice' => '0.00'
+		,'price' => array (
+			'hasPrice' => true
+			,'shopPrice' => array (
+				'normalPrice' => '45.95'
+				,'salePrice' => '0.00'
+			)
+			,'postagePrice' => array (
+				'domesticPrice' => '0.00'
+				,'internationalPrice' => '45.00'
+			)
+		)
 		,'Testimonials' => array (
-				'Quotes' => array (
-					'<i>"It was so lovely to see you Amanda and your beautiful new candle range they’re amazing.</i>" - Maz Rukz '
-					,'<i>"It certainly was amazing, and special for us all to be in the same place together, connecting, yaking, laughing, made my heart sing! Lighting my very special new Amanda candles today.</i>" - Gerry Crow'
-					,'<i>"I’m loving them. Best purchase I tell you and they smell so good you could eat them.</i>" - Kat Simicevic'
-					,'<i>"Beautiful Smelling Candles for Every situation.. Amanda Halls Awesome Collection.</i>" - Terry Virgina'
-					,'<i>"How blessed am I to receive these candles in the mail today. So grateful for all the wonderful gifts in life!! Wishing you all magic miracles every day. ❤</i>" - Magic Maree'
+			'Quotes' => array (
+				'<i>"It was so lovely to see you Amanda and your beautiful new candle range they’re amazing.</i>" - Maz Rukz '
+				,'<i>"It certainly was amazing, and special for us all to be in the same place together, connecting, yaking, laughing, made my heart sing! Lighting my very special new Amanda candles today.</i>" - Gerry Crow'
+				,'<i>"I’m loving them. Best purchase I tell you and they smell so good you could eat them.</i>" - Kat Simicevic'
+				,'<i>"Beautiful Smelling Candles for Every situation.. Amanda Halls Awesome Collection.</i>" - Terry Virgina'
+				,'<i>"How blessed am I to receive these candles in the mail today. So grateful for all the wonderful gifts in life!! Wishing you all magic miracles every day. ❤</i>" - Magic Maree'
 			)
 			,'Samples' => array (
 				''
@@ -136,8 +206,17 @@ So I suggested we print the meaning on the bottom of the card.
 This made teaching so easy for me and the students love it.
 Reading the Tarot became easy and they were confident with ability quickly.
 Our new book is easy to work with and makes it easy to reference when performing a reading.</p>'
-		,'normalPrice' => '49.95'
-		,'salePrice' => '0.00'
+		,'price' => array (
+			'hasPrice' => true
+			,'shopPrice' => array (
+				'normalPrice' => '49.95'
+				,'salePrice' => '0.00'
+			)
+			,'postagePrice' => array (
+				'domesticPrice' => '0.00'
+				,'internationalPrice' => '45.00'
+			)
+		)
 		,'Testimonials' => array (
 			'Quotes' => array (
 				''
@@ -153,8 +232,17 @@ Our new book is easy to work with and makes it easy to reference when performing
 	,'productImage' => 'img/products/psychic/candles/secrets/large_secrets_candle.jpg'
 	,'productImageText' => ''
 	,'productDescription' => 'In today’s fast moving world our lives move so fast and memories fade just as quick. We all have some old tapes locked in a cupboard that we are going to do something with. Well today is the day . Get them out sort through them. What is special and you want to keep for future generations. They make a lovely gift to give. Being able to share memories from family history, special occasions, weddings, christenings, or even the family just having fun. We can transfer old VHS and Super 8 to CD, DVD, SD Card, or USB memory stick.'
-	,'normalPrice' => '57.95'
-	,'salePrice' => '0.00'
+	,'price' => array (
+			'hasPrice' => true
+			,'shopPrice' => array (
+				'normalPrice' => '57.95'
+				,'salePrice' => '0.00'
+			)
+			,'postagePrice' => array (
+				'domesticPrice' => '0.00'
+				,'internationalPrice' => '45.00'
+			)
+		)
 	,'Testimonials' => array (
 			'Quotes' => array (
 				'<i>"It was so lovely to see you Amanda and your beautiful new candle range they’re amazing.</i>" - Maz Rukz '
@@ -174,8 +262,17 @@ Our new book is easy to work with and makes it easy to reference when performing
 	,'productImage' => 'img/products/psychic/candles/secrets/3_small_secrets_candle.jpg'
 	,'productImageText' => ''
 	,'productDescription' => 'In today’s fast moving world our lives move so fast and memories fade just as quick. We all have some old tapes locked in a cupboard that we are going to do something with. Well today is the day . Get them out sort through them. What is special and you want to keep for future generations. They make a lovely gift to give. Being able to share memories from family history, special occasions, weddings, christenings, or even the family just having fun. We can transfer old VHS and Super 8 to CD, DVD, SD Card, or USB memory stick.'
-	,'normalPrice' => '49.95'
-	,'salePrice' => '0.00'
+	,'price' => array (
+			'hasPrice' => true
+			,'shopPrice' => array (
+				'normalPrice' => '49.95'
+				,'salePrice' => '0.00'
+			)
+			,'postagePrice' => array (
+				'domesticPrice' => '0.00'
+				,'internationalPrice' => '45.00'
+			)
+		)
 	,'Testimonials' => array (
 			'Quotes' => array (
 				'<i>"It was so lovely to see you Amanda and your beautiful new candle range they’re amazing.</i>" - Maz Rukz '
